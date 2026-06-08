6444739aa1b3ca07c678f6b8e92201f6d209dc2f
Temp-2702 <Temp-2702@TEKWISSEN.HUB>
2026-06-08 14:34:16 +0530

    Remove remaining frontend source files

--- frontend/src/lib/socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-
-export const socket = io(BACKEND_URL, {
-    path: "/api/socket.io",
-    transports: ["websocket", "polling"],
-    autoConnect: true,
-});
